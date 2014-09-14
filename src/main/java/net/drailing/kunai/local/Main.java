56a8985a686638a7706b30420c6f3cf8f155fff1
Christian Dreier <dreiling@drailing.net>
2014-09-14 23:07:19 +0200

    added simple cli

--- src/main/java/net/drailing/kunai/local/Main.java
+++ src/main/java/net/drailing/kunai/local/Main.java
@@ -1,12 +1,35 @@
 package net.drailing.kunai.local;
 
+import java.net.UnknownHostException;
+
 import net.drailing.kunai.local.ui.UI;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		new UI();
+		if(args.length == 2){
+			
+			try {
+				KunaiSocket socket = new KunaiSocket(Integer.valueOf(args[1]));
+				socket.start();
+				
+				KunaiWebServer webserver = new KunaiWebServer(Integer.valueOf(args[0]), Integer.valueOf(args[1]));
+				webserver.startServer();
+				
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(0);
+			}
+
+			
+		}else{
+			new UI();
+		}
 		
 	}
 
